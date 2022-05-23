e0c0e1dd09074e11d588450bfb182b2dbefb0466
Евгений <ugend84@yandex.ru>
2022-05-23 17:44:57 +0300

    Первый коммит

---
+++ src/main/java/pro/sky/TestQuestions/TestQuestionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.TestQuestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestQuestionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/TestQuestions/TestQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.TestQuestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestQuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
